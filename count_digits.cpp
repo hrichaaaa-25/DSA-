7eca35efe73ea6edfbd9a7b6c4ef59f538ec9718
Hricha Samanta <81.g.hrichasamanta@gmail.com>
2026-02-18 06:31:56 +0530

    Add files via upload

---
+++ count_digits.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+     cin >> n;
+     int count =0;
+      while (n> 0)
+      { 
+       
+        count=count +1;
+        n =n /10;
+      }
+    printf("%d", count);
+    
+     return 0;
+}
